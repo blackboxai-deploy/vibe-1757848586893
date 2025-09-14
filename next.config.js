d866ee084197eaa8a39b765e7a15db9f6f3cf0e3
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-14 16:46:29 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+  env: {
+    CUSTOM_KEY: 'my-value',
+  },
+}
+
+module.exports = nextConfig
